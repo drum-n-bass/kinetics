f29e40be137d6ceb7b266d7c32f17b7c62235658
Oori <o@pluggable.biz>
2021-05-31 00:49:02 +0300

    feat(config): parse "random(x,y)" and replace with float

--- src/util/configParser.js
+++ src/util/configParser.js
@@ -1,4 +1,20 @@
 export default obj => {
 
-	return obj;
+	// Transform "random(min,max)" to a number
+	const prefix = "random";
+	const regex = new RegExp(`"random\\(([0-9]*[.])?[0-9]+,([0-9]*[.])?[0-9]+\\)"`, "ig");
+	const toFloat = value => parseFloat(value.replace(/^\D+/g, ''));	// convert contaminated string to float
+
+	let str = JSON.stringify(obj);	// stringify and regex, instead of interating the object+array values. both work..
+	str = str.replaceAll(regex, s => {
+		const min = toFloat(s.split(',')[0]);
+		const max = toFloat(s.split(',')[1]);
+		return Math.random() * (max - min) + min;
+	});
+
+	// <more transformation here>
+
+	const result = JSON.parse(str);
+	// console.log(result);
+	return result;
 }
